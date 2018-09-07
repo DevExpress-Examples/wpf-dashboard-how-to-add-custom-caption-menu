935a9ecd0b186c99c9c6fcce4909b7290ba3cd01
DevExpressExampleBot <fakeEmail>
2018-09-07 13:52:41 +0300

    Source auto update [skip ci]

---
+++ VB/WpfDashboardCustomCaptionMenu/MainWindow.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports DevExpress.DashboardCommon
+Imports System.Windows
+
+Namespace WpfDashboardCustomCaptionMenu
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+            Dim dashboard As New Dashboard()
+            Dim group As New DashboardItemGroup()
+            dashboard.Groups.Add(group)
+            dashboard.Items.AddRange(New GridDashboardItem() With {.Group = group}, New ChartDashboardItem() With {.Group = group}, New TreemapDashboardItem())
+            dashboardControl1.Dashboard = dashboard
+        End Sub
+    End Class
+End Namespace
